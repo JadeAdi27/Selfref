79e3c2ed05ff1689711ee8f8a8b780b7405f3ed6
Manvaditya <eurekajade27@gmail.com>
2024-06-07 17:02:51 +0530

    third commit added hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-
+import Hero from './Components/Hero';
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <Footer />
     </>
   );
